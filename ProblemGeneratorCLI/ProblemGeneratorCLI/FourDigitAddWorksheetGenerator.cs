4f4d509fc418e89fc5ec085e1c3b623ee92bae78
Chaney Fonseca <chaneyfonseca@gmail.com>
2022-06-06 19:45:27 -0500

    tweak add and subtract worksheet programs to include a gradual tapering of number lengths

--- ProblemGeneratorCLI/ProblemGeneratorCLI/FourDigitAddWorksheetGenerator.cs
+++ ProblemGeneratorCLI/ProblemGeneratorCLI/FourDigitAddWorksheetGenerator.cs
@@ -10,9 +10,17 @@ namespace ProblemGeneratorCLI
             Console.WriteLine("--------------------------------");
             Console.WriteLine();
             const int upperBound = 10000;
+            const int mediumBound = 1000;
             const int numProblems = 10;
 
             var random = new Random();
+
+            for (var i = 0; i < numProblems; ++i)
+            {
+                var firstInt = random.Next(mediumBound);
+                var secondInt = random.Next(mediumBound);
+                Console.WriteLine($"{firstInt} + {secondInt} = ____ ?");
+            }
 
             for (var i = 0; i < numProblems; ++i)
             {
--- ProblemGeneratorCLI/ProblemGeneratorCLI/SubtractWithRegroupingGenerator.cs
+++ ProblemGeneratorCLI/ProblemGeneratorCLI/SubtractWithRegroupingGenerator.cs
@@ -10,9 +10,33 @@ namespace ProblemGeneratorCLI
             Console.WriteLine("-----------------------------------------------");
             Console.WriteLine();
             const int upperBound = 10000;
+            const int mediumBound = 1000;
+            const int lowBound = 100;
             const int numProblems = 10;
 
             var random = new Random();
+
+            for (var i = 0; i < numProblems; ++i)
+            {
+                var firstInt = random.Next(lowBound);
+                var secondInt = random.Next(lowBound);
+
+                if (firstInt > secondInt)
+                    Console.WriteLine($"{firstInt} - {secondInt} = ____ ?");
+                else
+                    Console.WriteLine($"{secondInt} - {firstInt} = ____ ?");
+            }
+
+            for (var i = 0; i < numProblems; ++i)
+            {
+                var firstInt = random.Next(mediumBound);
+                var secondInt = random.Next(mediumBound);
+
+                if (firstInt > secondInt)
+                    Console.WriteLine($"{firstInt} - {secondInt} = ____ ?");
+                else
+                    Console.WriteLine($"{secondInt} - {firstInt} = ____ ?");
+            }
 
             for (var i = 0; i < numProblems; ++i)
             {
