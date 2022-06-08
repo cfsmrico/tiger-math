7113eac5be899c4e7a33cbbb960c7a2407af0ea2
Chaney Fonseca <chaneyfonseca@gmail.com>
2022-06-08 18:49:32 -0500

    add rounding generator

--- ProblemGeneratorCLI/ProblemGeneratorCLI/Program.cs
+++ ProblemGeneratorCLI/ProblemGeneratorCLI/Program.cs
@@ -6,8 +6,9 @@ namespace ProblemGeneratorCLI
     {
         static void Main(string[] args)
         {
-            FourDigitAddWorksheetGenerator.Run();
-            SubtractWithRegroupingGenerator.Run();
+            //FourDigitAddWorksheetGenerator.Run();
+            //SubtractWithRegroupingGenerator.Run();
+            RoundNumbersGenerator.Run();
 
             Console.WriteLine();
             Console.WriteLine("DONE");
---
+++ ProblemGeneratorCLI/ProblemGeneratorCLI/RoundNumbersGenerator.cs
@@ -0,0 +1,33 @@
+﻿
+namespace ProblemGeneratorCLI
+{
+    internal class RoundNumbersGenerator
+    {
+        static readonly Random random = new();
+
+        internal static void GenerateAndWriteMsg(int roundingTo = 100, int upperBound = 100, int numProblems = 10)
+        {
+
+            for (var i = 0; i < numProblems; ++i)
+            {
+                var x = random.Next(upperBound);
+                Console.WriteLine($" What is {x} rounded to the nearest {roundingTo}?");
+            }
+        }
+
+        internal static void Run()
+        {
+            Console.WriteLine();
+            Console.WriteLine("     Round numbers to nearest 10 and 100");
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine();
+
+            GenerateAndWriteMsg(roundingTo: 10, numProblems: 5);
+            GenerateAndWriteMsg(roundingTo: 10, numProblems: 5, upperBound: 500);
+            GenerateAndWriteMsg(roundingTo: 100, numProblems: 5);
+            GenerateAndWriteMsg(roundingTo: 100, numProblems: 5, upperBound: 500);
+            GenerateAndWriteMsg(roundingTo: 100, numProblems: 5, upperBound: 1000);
+        }
+    }
+}
+
